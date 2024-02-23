f2412a9b8842e5b4d15a6cd4f3e1628ba29ff245
Velshh <pkot2003@gmail.com>
2024-02-23 16:29:08 +0100

    add passwords generating

--- src/main/java/com/example/passwordhamster/PasswordGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.example.passwordhamster;
-
-public class PasswordGenerator {
-    //public String generate(int length, boolean containNumbers, boolean containSpecialChars, boolean containUppercaseOrLowercaseLetters) {
-
-    //}
-}
--- src/main/java/com/example/passwordhamster/PasswordGeneratorController.java
+++ src/main/java/com/example/passwordhamster/PasswordGeneratorController.java
@@ -5,6 +5,24 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import java.util.List;
+import java.util.Random;
+
+enum Errors {
+    LENGTH_IS_NOT_A_NUMBER("LENGTH IS NOT A NUMBER"),
+    LENGTH_EMPTY("LENGTH FIELD CANNOT BE EMPTY"),
+    LENGTH_NOT_PROPERLY_FORMATTED("WRONG LENGTH FORMAT");
+
+    private final String error;
+
+    Errors(String error) {
+        this.error = error;
+    }
+
+    public String getErrorMessage() {
+        return error;
+    }
+}
 
 public class PasswordGeneratorController {
     @FXML
@@ -37,37 +55,92 @@ public class PasswordGeneratorController {
         try {
             int length = Integer.parseInt(input);
 
-            if(length > 50) {
+            if (length > 50) {
                 passwordLengthField.setText("50");
-            }else {
-                if(!errorTextField.getText().isEmpty()) errorTextField.setText("");
+            } else {
+                if (!errorTextField.getText().isEmpty()) errorTextField.setText("");
             }
 
-            if(input.startsWith("0") || input.startsWith("-")) {
+            if (input.startsWith("0") || input.startsWith("-")) {
                 errorTextField.setText(Errors.LENGTH_NOT_PROPERLY_FORMATTED.getErrorMessage());
             }
 
-        }catch(NumberFormatException ex) {
-            if(!input.isEmpty()) {
+        } catch (NumberFormatException ex) {
+            if (!input.isEmpty()) {
                 errorTextField.setText(Errors.LENGTH_IS_NOT_A_NUMBER.getErrorMessage());
-            }else {
+            } else {
                 errorTextField.setText(Errors.LENGTH_EMPTY.getErrorMessage());
             }
         }
     }
-}
 
-enum Errors {
-    LENGTH_IS_NOT_A_NUMBER("LENGTH IS NOT A NUMBER"),
-    LENGTH_EMPTY("LENGTH FIELD CANNOT BE EMPTY"),
-    LENGTH_NOT_PROPERLY_FORMATTED("WRONG LENGTH FORMAT");
-
-    private final String error;
-    Errors(String error) {
-        this.error = error;
+    @FXML
+    public void onGenerateButtonClick() {
+        if(!passwordLengthField.getText().isEmpty()) {
+            generatedPasswordField.setText(
+                    generatePassword(Integer.parseInt(passwordLengthField.getText()),
+                            shouldPasswordContainSpecialCharactersCheckBox.isSelected(),
+                            shouldPasswordContainNumbersCheckBox.isSelected(),
+                            shouldPasswordContainLowercaseAndUppercaseLettersCheckBox.isSelected())
+            );
+            errorTextField.setText("");
+        }
     }
 
-    public String getErrorMessage() {
-        return error;
+    private String generatePassword(int length, boolean canContainSpecialChars, boolean canContainNumbers, boolean canContainUpperLowercaseLetters) {
+        StringBuilder generatedPassword = new StringBuilder();
+        Random random = new Random();
+
+        List<String> letters = List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z");
+        List<String> specialCharacters =  List.of(".", "@", "*", "#", "%", "!", "?", ",", ":");
+        List<String> numbers = List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        List<String> upperLowercaseLetters = List.of("a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z");
+
+
+
+        while (generatedPassword.length() < length) {
+            while(true) {
+                if (canContainSpecialChars) {
+
+                    if (decide()) {
+                        generatedPassword.append(specialCharacters.get(random.nextInt(specialCharacters.size())));
+                        break;
+                    }
+                }else {
+                    generatedPassword.append(letters.get(random.nextInt(letters.size())));
+                    break;
+                }
+
+                if(canContainNumbers) {
+
+                    if(decide()) {
+                        generatedPassword.append(numbers.get(random.nextInt(numbers.size())));
+                        break;
+                    }
+                }else {
+                    generatedPassword.append(letters.get(random.nextInt(letters.size())));
+                    break;
+                }
+
+                if(canContainUpperLowercaseLetters) {
+
+                    if(decide()) {
+                        generatedPassword.append(upperLowercaseLetters.get(random.nextInt(upperLowercaseLetters.size())));
+                        break;
+                    }
+                }else {
+                    generatedPassword.append(letters.get(random.nextInt(letters.size())));
+                    break;
+                }
+            }
+        }
+        return generatedPassword.toString();
+    }
+
+    private boolean decide() {
+        int range = 2;
+        Random random = new Random();
+
+        return random.nextInt(range) == 1;
     }
 }
